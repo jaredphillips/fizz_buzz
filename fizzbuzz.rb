55cae9597bfd03676e9e6a6e65f5af4819c60675
jaredphillips <jaredtphillips@me.com>
2013-04-01 20:15:01 -0400

    Fizzbuzz

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+number = 0
+
+while number <= 100
+  if (number % 3 == 0) && (number % 5 == 0)
+	puts "#{number} is Fizzbuzz"
+  elsif number % 3 == 0 
+	puts "#{number} is Fizz"
+  elsif number % 5 == 0
+	puts "#{number} is Buzz"
+  else
+	puts number
+  end
+  number = number + 1
+end
+
